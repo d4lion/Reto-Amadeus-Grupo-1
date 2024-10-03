c04217c4eb46de614f546821f50babc7180bd265
d4lion <d4lion.dev@gmail.com>
2024-10-02 23:54:50 -0500

    :recycle: refactor: components and imports alias for navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuNavegacionComponent } from './pages/menu-navegacion/menu-navegacion.component';
+import { MenuNavegacionComponent } from '@components/menu-navegacion/menu-navegacion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuNavegacionComponent ],
+  imports: [RouterOutlet, MenuNavegacionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AmadeusAngular';
